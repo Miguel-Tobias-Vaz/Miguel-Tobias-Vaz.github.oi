7b9d96f4a44821dfde74e6921832595d2dc983ee
Miguel Vaz <tobiasmiguel007@gmail.com>
2026-04-23 14:18:42 -0300

    versão-portfolio 0.1.1

---
+++ main.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".menu a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 140;
+    const sectionHeight = section.clientHeight;
+
+    if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
